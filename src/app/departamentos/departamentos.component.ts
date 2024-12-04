9da6196f3a6a10c9db7206c30c136f56b280ba73
Schwarzesmak <mat.gonzalez6@duocuc.cl>
2024-12-04 01:17:24 -0300

    a

--- src/app/departamentos/departamentos.component.ts
+++ src/app/departamentos/departamentos.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-departamentos',
-  standalone: true,  // Este es un componente independiente
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './departamentos.component.html',
   styleUrls: ['./departamentos.component.scss']
 })
 export class DepartamentosComponent implements OnInit {
-  departamentos: any[] = [];  // Lista de departamentos simulada
-
-  // Datos para un nuevo departamento
+  departamentos: any[] = [];
   nuevoDepto: any = {
     CodDepto: '',
     Piso: '',
@@ -28,65 +26,43 @@ export class DepartamentosComponent implements OnInit {
     NumHab: 1
   };
 
-  constructor() {}
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    // Aquí simulamos la carga de los departamentos (sin backend)
-    this.obtenerDepartamentos();  // Simulamos la carga de departamentos al iniciar
+    this.obtenerDepartamentos();  // Llama al método para cargar los departamentos al iniciar
   }
 
   obtenerDepartamentos(): void {
-    // Simulación de obtener datos, en este caso usamos un arreglo estático
-    this.departamentos = [
-      {
-        CodDepto: 'A101',
-        Piso: '1',
-        Numero: '01',
-        Estado: 'Disponible',
-        RutProp: '12345678-9',
-        Arrendado: false,
-        RutArre: '',
-        FechaIniC: '',
-        FechaFinC: '',
-        Observacion: '',
-        NumHab: 2
-      },
-      {
-        CodDepto: 'A102',
-        Piso: '1',
-        Numero: '02',
-        Estado: 'Arrendado',
-        RutProp: '98765432-1',
-        Arrendado: true,
-        RutArre: '22334455-6',
-        FechaIniC: '2023-01-01',
-        FechaFinC: '2024-01-01',
-        Observacion: 'Contrato vigente',
-        NumHab: 3
-      }
-    ];
+    this.apiService.getDepartamentos().subscribe((data: any) => {
+      this.departamentos = data.departamentos;
+    });
   }
 
   crearDepartamento(): void {
-    // Simula la creación de un departamento sin backend
-    console.log('Nuevo departamento:', this.nuevoDepto);
-
-    // Agrega el nuevo departamento a la lista
-    this.departamentos.push({ ...this.nuevoDepto });
-
-    // Limpiar el formulario
-    this.nuevoDepto = {
-      CodDepto: '',
-      Piso: '',
-      Numero: '',
-      Estado: 'Disponible',
-      RutProp: '',
-      Arrendado: false,
-      RutArre: '',
-      FechaIniC: '',
-      FechaFinC: '',
-      Observacion: '',
-      NumHab: 1
-    };
+    // Enviar los datos al backend para crear el nuevo departamento
+    this.apiService.createDepartamento(this.nuevoDepto).subscribe({
+      next: (response) => {
+        console.log('Departamento creado con éxito');
+        // Recargar la lista de departamentos después de la creación
+        this.obtenerDepartamentos(); // Actualiza los departamentos mostrando el nuevo
+        // Limpiar el formulario si deseas
+        this.nuevoDepto = {
+          CodDepto: '',
+          Piso: '',
+          Numero: '',
+          Estado: 'Disponible',
+          RutProp: '',
+          Arrendado: false,
+          RutArre: '',
+          FechaIniC: '',
+          FechaFinC: '',
+          Observacion: '',
+          NumHab: 1
+        };
+      },
+      error: (err) => {
+        console.error('Error al crear departamento:', err);
+      }
+    });
   }
 }
